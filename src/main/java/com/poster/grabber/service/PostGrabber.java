ed03196d0fb8347219ed6f16f468ea66dfd1f393
Gurpreet Singh Saini <30748982+guri27@users.noreply.github.com>
2017-08-12 10:41:42 +0530

    Added ability to create directories and move file into it.

--- src/main/java/com/poster/grabber/service/PostGrabber.java
+++ src/main/java/com/poster/grabber/service/PostGrabber.java
@@ -21,6 +21,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Calendar;
 
+import org.apache.commons.io.FileExistsException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -45,13 +48,26 @@ public class PostGrabber {
 	private static final String API_KEY = "682695192e252905a45c46ff9e0011a5";
 	private static final String SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&query=";
 	private static final String POSTER_URL = "http://image.tmdb.org/t/p/w600";
+	private static final String FILTER_NAME_REGEX = "[\\d\\'(']";
 
+	/**
+	 * This method gets all files of the directory
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
 	private DirectoryStream<Path> getFiles() throws IOException {
 		Path path = Paths.get(this.directory);
 		DirectoryStream<Path> dirStream = Files.newDirectoryStream(path);
 		return dirStream;
 	}
 
+	/**
+	 * This method verifies, if thumbnail already exists
+	 * 
+	 * @param path
+	 * @return
+	 */
 	private boolean isThumbnailExist(Path path) {
 		DirectoryStream<Path> dirStream;
 		try {
@@ -68,17 +84,37 @@ public class PostGrabber {
 		return false;
 	}
 
-	private String filterName(String name) {
+	/**
+	 * This method remove extension name from file
+	 * 
+	 * @param name
+	 * @return
+	 */
+	private String filterExt(String name) {
 		String filteredName = null;
-		int endIndex = name.indexOf(YEAR_PREFIX);
-		if (endIndex > -1) {
-			filteredName = name.substring(0, endIndex);
-		} else {
-			filteredName = name;
-		}
+		filteredName = FilenameUtils.removeExtension(name);
 		return filteredName;
 	}
 
+	/**
+	 * This method filters name, so that it can be searched
+	 * 
+	 * @param name
+	 * @return
+	 */
+	private String filterName(String name) {
+		String filteredName = null;
+		filteredName = name.replace(".", " ");
+		filteredName = filteredName.split(FILTER_NAME_REGEX)[0];
+		return filteredName;
+	}
+
+	/**
+	 * This method search the movie name and gets details
+	 * 
+	 * @param searchName
+	 * @return
+	 */
 	private MovieModel searchName(String searchName) {
 		String name = filterName(searchName);
 		System.out.println("Getting details for: " + name);
@@ -94,6 +130,12 @@ public class PostGrabber {
 		return res;
 	}
 
+	/**
+	 * This method downloads poster
+	 * 
+	 * @param movie
+	 * @param path
+	 */
 	private void downloadPoster(MovieModel movie, Path path) {
 		try {
 			System.out.println("Downloading poster for:" + movie.getOriginal_title());
@@ -107,23 +149,67 @@ public class PostGrabber {
 		}
 	}
 
+	/**
+	 * This method renames the diretory
+	 * 
+	 * @param movie
+	 * @param sourcePath
+	 */
 	private void renameDir(MovieModel movie, Path sourcePath) {
+		Path destPath = null;
 		try {
 			System.out.println("Renaming directory: " + movie.getOriginal_title());
 			Calendar cal = Calendar.getInstance();
 			cal.setTime(movie.getRelease_date());
 			String dest = this.directory + File.separator + movie.getOriginal_title() + " " + YEAR_PREFIX
 					+ cal.get(Calendar.YEAR) + YEAR_POSTFIX;
-			File source = sourcePath.toFile();
-			File destFile = new File(dest);
-			source.renameTo(destFile);
+			destPath = Paths.get(dest);
+			FileUtils.moveDirectory(sourcePath.toFile(), destPath.toFile());
 			System.out.println("Successfully renamed to: " + dest);
+		} catch (FileExistsException e) {
+			System.out.println("Successfully renamed to: " + destPath);
 		} catch (Exception e) {
 			System.out.println("Exception while doing operation");
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * This method creates directory and moves the file into it
+	 * 
+	 * @param movie
+	 * @param sourceFile
+	 */
+	private void createDirAndMove(MovieModel movie, Path sourceFile) {
+		System.out.println("Creating directory for : " + movie.getOriginal_title());
+
+		// Update dir name, if contains /,?,:
+		String updateMovieName = movie.getOriginal_title().replaceAll(":", "");
+		movie.setOriginal_title(updateMovieName);
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(movie.getRelease_date());
+		Path dirPath = Paths.get(this.directory + File.separator + movie.getOriginal_title() + " " + YEAR_PREFIX
+				+ cal.get(Calendar.YEAR) + YEAR_POSTFIX);
+		Path path = Paths.get(this.directory + File.separator + movie.getOriginal_title() + " " + YEAR_PREFIX
+				+ cal.get(Calendar.YEAR) + YEAR_POSTFIX + File.separator + sourceFile.getFileName());
+		try {
+			Files.createDirectory(dirPath);
+			downloadPoster(movie, dirPath);
+			// Move file
+			FileUtils.moveFile(sourceFile.toFile(), path.toFile());
+
+		} catch (IOException e) {
+			System.out.println("Exception while doing operation");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * This method updates the selected directory with thumbnails
+	 * 
+	 * @throws IOException
+	 */
 	public void update() throws IOException {
 		System.out.println("Grabbing posters in directory: " + this.directory);
 		DirectoryStream<Path> dirStream = getFiles();
@@ -142,10 +228,24 @@ public class PostGrabber {
 				} else {
 					System.out.println("Thumbnail already exist for: " + dir.getFileName().toString());
 				}
+			} else {
+				String name = filterExt(dir.getFileName().toString());
+				String filteredName = filterName(name);
+				MovieModel movie = searchName(filteredName);
+				if (movie != null && movie.getPoster_path() != null) {
+					createDirAndMove(movie, dir);
+				} else {
+					System.out.println("Couldn't find details for " + dir.getFileName().toString());
+				}
 			}
 		});
 	}
 
+	/**
+	 * This method deletes all the thumbnails existing in that directory
+	 * 
+	 * @throws IOException
+	 */
 	public void delete() throws IOException {
 		System.out.println("Grabbing posters in directory: " + this.directory);
 		DirectoryStream<Path> dirStream = getFiles();
@@ -165,4 +265,13 @@ public class PostGrabber {
 			}
 		});
 	}
+	
+	/*public static void main(String[] args) {
+		PostGrabber postGrabber = new PostGrabber("D:\\test");
+		try {
+			postGrabber.update();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}*/
 }
